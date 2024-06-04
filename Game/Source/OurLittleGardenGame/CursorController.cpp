50ecaa2e6af5804c95c995ff9cc502064caf62ea
Lowenna Whitehead <lw260328@falmouth.ac.uk>
2024-06-04 09:26:20 +0100

    misc: Mouse partial
    most of this has been deleted. this shite is broken af! lemme quickfix

---
+++ Game/Source/OurLittleGardenGame/CursorController.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CursorController.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+// Sets default values
+ACursorController::ACursorController()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACursorController::BeginPlay()
+{
+	Super::BeginPlay();
+	GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, TEXT("Scene is Working"));
+}
+
+void ACursorController::Interaction(const FInputActionValue& Value)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, TEXT("interact event triggered"));
+}
+
+// Called every frame
+void ACursorController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACursorController::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(InteractAction, ETriggerEvent::Triggered, this, &ACursorController::Interaction);
+	}
+}
+
---
+++ Game/Source/OurLittleGardenGame/CursorController.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "InputAction.h"
+#include "CursorController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class OURLITTLEGARDENGAME_API ACursorController : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACursorController();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//cursor visibility
+	bool bShowMouseCursor = true;
+
+	//xUPROPERTY(EditAnywhere, BlueprintReadWrite, category = "Cursor")
+	//xEMouseCursorType MouseCursor = EMouseCursorType: Default;
+
+	//Character mapping context for controls
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "EnhancedInput")
+	UInputMappingContext* InputMappingContext;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "EnhancedInput")
+	class UInputAction* InteractAction;
+
+	void Interaction(const FInputActionValue& Value);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
