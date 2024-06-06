b26cd372f154df162d1841ccf7bbf85ac300e853
Harry Croker <hj.croker@gmail.com>
2024-06-06 18:07:03 +0100

    misc: Created USTRUCTs for responses and dialogue lines

--- Game/Source/OurLittleGardenGame/Private/CharacterEnumeration.cpp
+++ Game/Source/OurLittleGardenGame/Private/CharacterEnumeration.cpp
@@ -3,24 +3,6 @@
 
 #include "CharacterEnumeration.h"
 
-UENUM(BlueprintType)
-enum class ECharacter : uint8
-{
-	CHEF UMETA(DisplayName = "Chef"),
-	FLORIST UMETA(DisplayName = "Florist"),
-	MAILMAN UMETA(DisplayName = "Mailman")
-
-};
-
-UENUM(BlueprintType)
-enum class EEmotion : uint8
-{
-	HAPPY,
-	SAD,
-	ANGRY
-
-};
-
 CharacterEnumeration::CharacterEnumeration()
 {
 }
