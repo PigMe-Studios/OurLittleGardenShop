ea2f9a072f75782c022ecf9e6589ceb7baece392
Harry Croker <hj.croker@gmail.com>
2024-06-10 03:52:37 +0100

    feat: print dialogue line to log

--- Game/Source/OurLittleGardenGame/Private/Customer.cpp
+++ Game/Source/OurLittleGardenGame/Private/Customer.cpp
@@ -15,14 +15,20 @@ ACustomer::ACustomer()
 void ACustomer::BeginPlay()
 {
 	Super::BeginPlay();
+	UpdateDialogue(FName("NewRow"));
 	
 }
 
 bool ACustomer::UpdateDialogue(FName Name)
 {
-	FDialogueLine* Row = DIALOGUE_TABLE->FindRow<FDialogueLine>(Name, "");
-	Emotion = Row->CharacterEmotion;
-	// TODO: Add UI element to update, update it's text
+	if (FDialogueLine* Row = DIALOGUE_TABLE->FindRow<FDialogueLine>(Name, ""))
+	{
+		Emotion = Row->CharacterEmotion;
+		// TODO: Add UI element to update, update it's text
+		// TODO: Remove "Name" from struct, as Data-tables already have row names
+		UE_LOG(LogTemp, Warning, TEXT("Text: %s"), *Row->Content);
+		return true;
+	}
 	return false;
 }
 
