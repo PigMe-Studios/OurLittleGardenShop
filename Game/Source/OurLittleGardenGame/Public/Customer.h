79c0442570a55e107a2ab894fedba4658d83a843
Harry Croker <hj.croker@gmail.com>
2024-06-10 03:11:00 +0100

    misc: Set-up BP_Customer

---
+++ Game/Source/OurLittleGardenGame/Public/Customer.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CharacterEnumeration.h"
+#include "Customer.generated.h"
+
+UCLASS()
+class OURLITTLEGARDENGAME_API ACustomer : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACustomer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UDataTable* DIALOGUE_TABLE;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ECharacter Character;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EEmotion Emotion;
+};
